07eb148bc67b92b4360386f1c6e17044987eefbd
Cristian Cázares <cm_cjavier@hotmail.com>
2021-10-05 12:38:09 +0000

    [ENHANCEMENT] Convert a character to uppercase.

---
+++ minmay.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<ctype.h>
+
+
+int main(){
+    char c, may;
+    read(STDIN_FILENO, &c, 1);
+    may = toupper(c);
+    write(STDOUT_FILENO,&may,1);
+    return 0;
+}
